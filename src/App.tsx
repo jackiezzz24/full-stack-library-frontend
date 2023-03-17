84e422f8925b05b5b04be9f2032c4de1ce0bf05e
Jiaqi Zhao <jiaqi.zhao24@gmail.com>
2023-03-16 22:32:40 -0700

    add search books page with pagination

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import { Homepage } from './Layouts/Homepage/Homepage';
 import { Footer } from './Layouts/NavbarAndFooter/Footer';
 import { Navbar } from './Layouts/NavbarAndFooter/Navbar';
+import { SearchBooksPage } from './Layouts/SearchBooksPage/SearchBooksPage';
 
 export const App = () => {
   return (
     <div>
       <Navbar/>
-      <Homepage/>
+      {/* <Homepage/> */}
+      <SearchBooksPage/>
       <Footer/>
     </div>
   );
