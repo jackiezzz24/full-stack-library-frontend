95757b3f129d69a80f9fc47b2e24112f94b7ac06
Jiaqi Zhao <jiaqi.zhao24@gmail.com>
2023-03-17 21:45:44 -0700

    add checkout page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { BookCheckoutPage } from './Layouts/BookCheckoutPage/BookCheckoutPage';
 import { Homepage } from './Layouts/Homepage/Homepage';
 import { Footer } from './Layouts/NavbarAndFooter/Footer';
 import { Navbar } from './Layouts/NavbarAndFooter/Navbar';
@@ -18,7 +19,10 @@ export const App = () => {
             <Homepage/>
           </Route>
           <Route path='/search'>
-          <SearchBooksPage/>
+            <SearchBooksPage/>
+          </Route>
+          <Route path='/checkout/:bookId'>
+            <BookCheckoutPage/>
           </Route>
         </Switch>
       </div>
