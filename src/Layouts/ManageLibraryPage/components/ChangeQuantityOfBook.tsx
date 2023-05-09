c2569f96ff0c32b6cc9e61234858475fd3abfb9a
Jiaqi Zhao <jiaqi.zhao24@gmail.com>
2023-05-09 23:51:13 +0800

    add openssl

--- src/Layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
+++ src/Layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
@@ -18,7 +18,7 @@ export const ChangeQuantityOfBook: React.FC<{ book: BookModel, deleteBook: any }
     }, [props.book.copies, props.book.copiesAvailable]);
 
     async function increaseQuantity() {
-        const url=`http://localhost:8080/api/admin/secure/increase/book/quantity/?bookId=${props.book?.id}`;
+        const url=`${process.env.REACT_APP_API}/admin/secure/increase/book/quantity/?bookId=${props.book?.id}`;
         const requestOptions = {
             method: 'PUT',
             headers: {
@@ -35,7 +35,7 @@ export const ChangeQuantityOfBook: React.FC<{ book: BookModel, deleteBook: any }
     }
 
     async function decreaseQuantity() {
-        const url=`http://localhost:8080/api/admin/secure/decrease/book/quantity/?bookId=${props.book?.id}`;
+        const url=`${process.env.REACT_APP_API}/admin/secure/decrease/book/quantity/?bookId=${props.book?.id}`;
         const requestOptions = {
             method: 'PUT',
             headers: {
@@ -52,7 +52,7 @@ export const ChangeQuantityOfBook: React.FC<{ book: BookModel, deleteBook: any }
     }
 
     async function deleteBook() {
-        const url=`http://localhost:8080/api/admin/secure/delete/book/?bookId=${props.book?.id}`;
+        const url=`${process.env.REACT_APP_API}/admin/secure/delete/book/?bookId=${props.book?.id}`;
         const requestOptions = {
             method: 'DELETE',
             headers: {
