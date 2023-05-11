cb297fb5d67d9ea48f19227a4aca38320b544da8
Jiaqi Zhao <jiaqi.zhao24@gmail.com>
2023-05-11 14:19:12 +0800

    feat: add payment features

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,6 +1,6 @@
 export const oktaConfig = {
-    clientId:'',
-    issuer:'https://dev-xxx.okta.com/oauth2/default',
+    clientId:'0oa8rtrsiou8WocGN5d7',
+    issuer:'https://dev-81873002.okta.com/oauth2/default',
     redirectUri: 'https://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
